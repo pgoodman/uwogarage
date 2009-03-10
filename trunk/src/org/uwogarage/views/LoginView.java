ac9a876cde831fb461ee0352458a5e40abc9731b
peter.goodman <peter.goodman@8898c818-eca5-11dd-b90f-f9a5cbf66604>
2009-03-10 19:46:06 +0000

    working on top menu bar and login view

---
+++ trunk/src/org/uwogarage/views/LoginView.java
@@ -0,0 +1,13 @@
+package org.uwogarage.views;
+
+import javax.swing.JFrame;
+
+public class LoginView extends View {
+
+    @Override
+    public <T> T view(JFrame f) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
--- trunk/src/org/uwogarage/views/View.java
+++ trunk/src/org/uwogarage/views/View.java
@@ -1,5 +1,24 @@
 package org.uwogarage.views;
 
-public interface View {
+import javax.swing.JFrame;
+
+/**
+ * View class, represents a single component within the interface. Extends
+ * SimpleGui, mostly out of laziness, as I am not interested in static importing
+ * the class everywhere.
+ * 
+ * @author Peter Goodman
+ *
+ */
+abstract public class View extends org.uwogarage.util.gui.SimpleGui {
     
+    /**
+     * Show the view. Views are meant to be able to respond with some sort of
+     * objects to the controllers calling them.
+     * 
+     * @param <T>
+     * @param f
+     * @return
+     */
+    abstract public <T> T view(JFrame f);
 }
