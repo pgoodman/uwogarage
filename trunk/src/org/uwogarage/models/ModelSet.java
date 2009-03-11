816b0a90c03782859e152084f5105bd1604a4c26
peter.goodman <peter.goodman@8898c818-eca5-11dd-b90f-f9a5cbf66604>
2009-03-11 21:48:07 +0000

    Changed ModelCollection to ModelSet, LoginView appears to be mostly complete now.

--- trunk/src/org/uwogarage/models/ModelSet.java
+++ trunk/src/org/uwogarage/models/ModelSet.java
@@ -1,27 +1,19 @@
 package org.uwogarage.models;
 
-import java.util.LinkedList;
-
+import java.util.HashSet;
 import org.uwogarage.util.functional.P;
 
-public class ModelCollection<T> extends LinkedList<T> {
+public class ModelSet<T> extends HashSet<T> {
     
     private static final long serialVersionUID = 1L;
 
-    /**
-     * Constructor.
-     */
-    public ModelCollection() {
-        
-    }
-    
     /**
      * Filter out specific models in the model collection.
      * @param filter_fn Filter predicate
      * @return
      */
-    public LinkedList<T> filter(P<T> filter_fn) {
-        ModelCollection<T> filtered_models = new ModelCollection<T>();
+    public ModelSet<T> filter(P<T> filter_fn) {
+        ModelSet<T> filtered_models = new ModelSet<T>();
         for(T model : this) {
             if(filter_fn.call(model)) {
                 filtered_models.add(model);
@@ -29,5 +21,17 @@ public class ModelCollection<T> extends LinkedList<T> {
         }
         
         return filtered_models;
+    }
+    
+    /**
+     * Filter out a specific model in a collection.
+     */
+    public T filterOne(P<T> filter_fn) {
+        for(T model : this) {
+            if(filter_fn.call(model)) {
+                return model;
+            }
+        }
+        return null;
     }
 }
